6d7d5356c846a76fd8eb61c87d8a0ce37411cffd
HarringtonR <ross.ludeke@gmail.com>
2019-06-10 11:58:19 -0500

    cards

--- source/Ball.js
+++ source/Ball.js
@@ -6,7 +6,7 @@ export default class Ball extends Component {
   componentWillMount(){
     this.position = new Animated.ValueXY(0,0);
     Animated.spring(this.position, {
-      toValue: {x: 190, y: 400 }
+      toValue: {x: 200, y: 500 }
     }).start();
   }
 
